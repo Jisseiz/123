03ff3eb96a9c452669b5060d3b4a07f65401c633
Jissei <Falcon1041998@gmail.com>
2022-10-13 17:19:26 +0300

    Homework3

---
+++ Lession3/Example1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+void CheckNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.Write("Да");
+    }
+    else Console.Write("Нет");
+}
+
+CheckNumber(number);
