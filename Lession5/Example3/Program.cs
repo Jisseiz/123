8dfd4304cca39f1eaa9519f341053912909c9a36
Jissei <Falcon1041998@gmail.com>
2022-10-20 11:50:14 +0300

    Homework5

---
+++ Lession5/Example3/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > max)
+    {
+        max = numbers[z];
+    }
+    if (numbers[z] < min)
+    {
+        min = numbers[z];
+    }
+}
+
+Console.WriteLine(max - min);
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
+    }
+}
