8b2a69e60e28b587ecc7253cdf8f04a3de2dbff3
Jissei <Falcon1041998@gmail.com>
2022-10-20 11:28:50 +0300

    add less9

---
+++ Lession9/Example1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Integers();
+
+
+
+
+void Integers()
+{
+    for (int i = 1; i <= N; i++)
+        Console.Write($" {i}");
+    return;
+    Integers();
+}
---
+++ Lession9/Example2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("N: ");
+int n = Convert.ToInt16(Console.ReadLine());
+Console.Write("M: ");
+int m = Convert.ToInt16(Console.ReadLine());
+CalculateSumm(m, n);
+
+int CalculateSumm(int m, int n)
+{
+    int start = m;
+    int end = n;
+    if (m > n)
+    {
+        start = n;
+        end = m;
+        Console.WriteLine(CalculateSumm(n, m));
+    }
+    return (end + start) * (end - start + 1) / 2;
+}
+
+
+
+
